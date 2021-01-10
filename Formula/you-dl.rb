b338915c39c6d580c1a13146b08d49cde9c797c2
L-oris <loris4526@gmail.com>
2021-01-10 17:23:59 +0100

    update shasum

--- Formula/you-dl.rb
+++ Formula/you-dl.rb
@@ -4,7 +4,7 @@ class YouDl < Formula
     desc "A tiny and easy to use YouTube downloader"
     homepage "https://github.com/l-oris/you-dl"
     url "https://github.com/l-oris/you-dl/releases/download/v0.1.0/you-dl-mac.tar.gz"
-    sha256 "bf9ee3803e71f10045ee39fb8c4b0a9a3327220a47957e886e13f13911e83b1d"
+    sha256 "b65c5d1517f0e688bdc9b18d656dad278145564331170f0038f74b0e42ac4f0e"
     version "0.1.0"
   
     def install
