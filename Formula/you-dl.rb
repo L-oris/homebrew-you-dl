48e58ce1790e130a308c847420131594597af168
L-oris <loris4526@gmail.com>
2021-01-11 22:22:29 +0100

    v0.2.0

--- Formula/you-dl.rb
+++ Formula/you-dl.rb
@@ -3,9 +3,9 @@
 class YouDl < Formula
     desc "A tiny and easy to use YouTube downloader"
     homepage "https://github.com/l-oris/you-dl"
-    url "https://github.com/l-oris/you-dl/releases/download/v0.1.0/you-dl-mac.tar.gz"
-    sha256 "b65c5d1517f0e688bdc9b18d656dad278145564331170f0038f74b0e42ac4f0e"
-    version "0.1.0"
+    url "https://github.com/l-oris/you-dl/releases/download/v0.2.0/you-dl-mac.tar.gz"
+    sha256 "0768aab1239006df9ee49cb824baa67bcfb4b6a0e1341a0cc9698ada491c40c3"
+    version "0.2.0"
   
     def install
       bin.install "you-dl"
